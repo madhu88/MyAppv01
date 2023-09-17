9ad14129c31240c01b5ad52122b357ce1d29fe4e
madhukar.gujjar <madhukar.gujjar@vodafone.com>
2023-09-17 19:50:03 +0100

    Adding app with rest template dependency.

--- app/src/main/java/com/myapp/MainActivity1.java
+++ app/src/main/java/com/myapp/MainActivity1.java
@@ -3,9 +3,15 @@ package com.myapp;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TimeUtils;
+import android.widget.TextView;
 
 import androidx.activity.ComponentActivity;
 import androidx.annotation.RequiresApi;
+
+import java.util.Calendar;
+import java.util.Date;
 
 @RequiresApi(api = Build.VERSION_CODES.P)
 public class MainActivity1 extends ComponentActivity {
@@ -13,6 +19,14 @@ public class MainActivity1 extends ComponentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("in onCreate", "started app for first time ever");
         setContentView(R.layout.activity_main);
+        populateTextData();
+    }
+
+    private void populateTextData() {
+        TextView textView = findViewById(R.id.helloText);
+        Date date = Calendar.getInstance().getTime();
+        textView.setText(date.toString());
     }
 }
